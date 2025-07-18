482c4a0d67bc8ae1cc8231602eef0b41201f3dbb
prabhat0204 <prabhat0204@gmail.com>
2025-07-18 08:39:05 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Hello from React! 👋</h1>
+      <p>This is a sample React component.</p>
+    </div>
+  )
+}
+
+export default App
